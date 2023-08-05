0018d18875443988e5f662a5bad8d0e9814684d7
Shair17 <jimmy_jhair17@hotmail.com>
2023-08-05 11:07:32 -0500

    primera semana

---
+++ app.js
@@ -0,0 +1,17 @@
+// Identifiquen que variables deben ser constantes (const) y que otras variables
+// deben ser (let)
+let nombre = 'Juan';
+let instituto = 'IDAT';
+let edad = 19;
+let nombreDeMascota = 'Scooby';
+let ciudad = 'Lima';
+
+console.log(
+	'Hola Mundo soy ' +
+		nombre +
+		' y estoy estudiando en ' +
+		instituto +
+		' y tengo ' +
+		edad +
+		' años'
+);

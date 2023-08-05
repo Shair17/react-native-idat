d0ad90a8e6029bb3ce1cd59d8bac8de9af6482b0
Shair17 <jimmy_jhair17@hotmail.com>
2023-08-05 12:58:53 -0500

    hackaton: primera semana

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function obtenerEdad(fecha = new Date()) {
 	return edad;
 }
 
+// Objetos
+
 let persona = {
 	nombre: 'Juan',
 	apellidos: 'Perez Chacón',
@@ -45,3 +47,32 @@ for (const mascota of persona.mascotas) {
 persona.edad = obtenerEdad(persona.fechaDeNacimiento);
 
 console.log(persona.edad);
+
+// Arreglos
+// es recomendable usar los arreglos para **agrupar** datos de un mismo tipo de dato
+let libros = ['Naranja Lima', 'Luna de Plutón', 'Patrones de Diseño'];
+let notasPrimeraUnidad = [14, 15, 10];
+let listaDeCosasQueHacer = [
+	{
+		completado: false,
+		todo: 'Salir en bici',
+	},
+	{
+		completado: false,
+		todo: 'Limpiar la casa',
+	},
+	{
+		completado: false,
+		todo: 'Lavar la ropa',
+	},
+	{
+		completado: false,
+		todo: 'Hacer ejercicio',
+	},
+];
+
+// Hackaton: Hacer uso de los objetos y arreglos
+// Representar listas de tareas para completar, adicionalmente agregar la fecha en la que se completo y poder marcar tareas como completadas o pendientes
+// Crear un sistema de registro de libros para una biblioteca, utilizar datos de los libros como autor, titulo, año de publicación, si el libro está prestado, etc
+// Hacer un registro de egresos e ingresos en el mes, representar cada gasto e ingreso con los recursos necesarios, esto tiene que representar un mes de egresos e ingresos de dinero
+// Hacer un registro de notas en un curso, 1U - 2U - 3U y al final sacamos el promedio de nota del curso
